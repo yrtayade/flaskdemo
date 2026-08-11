2942554ef54532affa67dbd2ddbfe7d2caddc645
Yogesh Tayade <yotayade@gmail.com>
2026-08-11 21:34:19 +0530

    Fix: bind to Railway PORT variable

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template
-
+import os
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return render_template('index.html')
 
-if __name__ == '__main__':    
-    app.run(host='0.0.0.0',port=8080, debug=True)
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
