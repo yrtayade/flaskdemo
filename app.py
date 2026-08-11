008912a497c67274d3743a55da037dd3938c4a0d
Yogesh Tayade <yotayade@gmail.com>
2026-08-11 12:16:03 +0530

    port

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ def hello_world():
     return render_template('index.html')
 
 if __name__ == '__main__':    
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0',port=8080, debug=True)
